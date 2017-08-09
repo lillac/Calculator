d90d18cd5453f9f787289460849570f8d5a10695
Yattaze <arthurhuia@gmail.com>
2017-08-09 13:54:58 +1200

    Update calcu.py
    Untested, but i was bored and you should lean how to use classes

--- calcu.py
+++ calcu.py
@@ -1,17 +1,33 @@
-a = float(input("1st Number:"))
-b = float(input("2nd NUmber:"))
-m = input("Enter m for *, d for /, s for -, a for +.")
-c = a * b
-y = a / b
-f = a + b
-h = a - b
-if m == "m":
-  print(c)
-elif m == "d":
-  print(y)
-elif m == "s":
-  print(h)
-elif m == "a":
-  print(f)
-else:
-  print("Error")
+class calc(object):
+	def __init__(num1, num2):
+		self.types = ['add', 'sub', 'mul', 'div']
+		self.type = 'add'
+		self.num1 = num1
+		self.num2 = num2
+		
+	def calc():
+		if (self.type not in self.types):
+			print('Error')
+			return
+		elif (self.type = 'add'):
+			return self.num1 + self.num2
+		elif (self.type = 'sub'):
+			return self.num1 - self.num2
+		elif (self.type = 'mul'):
+			return self.num1 * self.num2
+		elif (self.type = 'div'):
+			return self.num1 / self.num2
+		
+
+def main():
+	a = float(input("1st Number:"))
+	b = float(input("2nd NUmber:"))
+	m = input("Enter mul for *, div for /, sub for -, add for +.")
+	res = calc(a, b)
+	res.type = m
+	result = res.calc()
+	print(result)
+	
+if __name__ == "__main__":
+    main()
+
